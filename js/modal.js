c5cf71507bc7cbaa7de543bc758586212ddd363e
Dror Ventura <drorventura@gmail.com>
2014-06-01 20:56:24 +0300

    Code Refactored

--- js/modal.js
+++ js/modal.js
@@ -1,5 +1,6 @@
 var i = 1;
 var amountOfImages = 3;     // TODO change the size to the amount from DB
+var imagePath = "/costaRicaIsrael/img/event/";
 
 function setIndex(index)
 {
@@ -24,3 +25,19 @@ function toggleRight()
         $("#image").html("<img class='bigImage' src='/costaRicaIsrael/img/event/"+i+".jpg' />");
     }
 }
+
+$(document).ready(function() {
+
+    var container = $('<div />');
+
+    for(var j = 1; j <= amountOfImages; j++) {
+        $('<a />', {
+            id: "id" + j,
+            href: "#openModal",
+            onclick: "setIndex(" + j + ")",
+            html: "<img class='thumb' src='"+ imagePath + j +".jpg'>"
+        }).appendTo(container);
+    }
+
+    $('.imageTable').html(container);
+});
