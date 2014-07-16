1675a7200e0a548fcae69f1ea75f0fb52627057f
drorventura <ventura2005@gmail.com>
2014-07-16 14:11:31 +0300

    Added - Connection with database - Code Refactored

--- event.php
+++ event.php
@@ -1,98 +1,73 @@
 <!doctype html>
 <html>
 <head>
+    <meta charset="utf-8" />
+    <title>Costa Rica Israel</title>
     <link rel="stylesheet" href="/costaRicaIsrael/css/main.css">
     <link rel="stylesheet" href="/costaRicaIsrael/css/events.css">
     <link rel="stylesheet" href="/costaRicaIsrael/css/modal.css">
+
     <script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
     <script src="/costaRicaIsrael/js/modal.js"></script>
     <?php require './con_util.php' ?>
 
-    <meta charset="utf-8" />
-    <title>Costa Rica Israel</title>
 </head>
 
 <body>
-    <script src="/costaRicaIsrael/js/nav_bar.js"></script>
+    <?php require 'templates/navbar.php'?>
+    <?php include 'templates/modal.php' ?>
 
     <div id="container_center">
         <div class="container">
-
-<?php 
+            <?php
                 $id = isset($_GET["id"]) ? (int)$_GET["id"] : 0;
                 $type = isset($_GET["type"]) ? $_GET["type"] : 0;
 
                 if($id && $type) {
 
-                $con;
-                $numOfImages;
-                set_con($con);
+                    $con;
+                    set_con($con);
 
-                $columns = "id , name , date , text , number_of_pics";
+                    $columns = "id , name , date , text , number_of_pics";
 
-                $prepare_query = 'SELECT ' . $columns .
-                    ' FROM ' . ($type == 'events' ? "events" : "meetings") . '_en WHERE id=?';
+                    $prepare_query = 'SELECT ' . $columns .
+                                     ' FROM ' . ($type == 'events' ? "events" : "meetings") . '_en WHERE id=?';
 
-                if($query = $con->prepare($prepare_query)) {
+                    if($query = $con->prepare($prepare_query)) {
+                        $query->bind_param("i",$id);
+                        $query->execute();
+                        $query->bind_result($id_col,$name,$date,$text,$numOfImages);
+                        $query->fetch();
 
-                $query->bind_param("i",$id);
-                $query->execute();
-                $query->bind_result($id_col,$name,$date,$text,$numOfImages);
-                $query->fetch();
+                        if(!$id_col) {
+                            echo $type . ' is not found in db... please contact admin!';
+                            exit();
+                        }
 
-                if(!$id_col) {
-                    echo $type . ' is not found in db... please contact admin!';
-                    exit();
-                }
-
-            echo '<p class="events_tablesName">' . $name . '<br>' . $date . '</p><hr>
-            <p class="text">' . $text . '</p>';
+                        echo   '<p class="events_tablesName">' . $name . '<br>' . $date . '</p><hr>
+                                <p class="text">' . $text . '</p>';
 
                     } else {
-                        echo '<br> <br>';
-                        printf("\nproblem with statemt..: \n");
-                        echo 'This is query : ' .$query;
+                        echo "Error #20 - Problem with statement.";
                         exit();
                     }
+
                 } else {
-                    printf("Argument is missing..: \n");
+                    echo "Error #21 - Missing argument.";
                     exit();
                 }
-
-            echo '<br>
+            ?>
+            <br>
             <div class="imageTable"></div>
 
         </div>
     </div>
 
-    <script src="/costaRicaIsrael/js/footer.js"></script>
+    <?php
+        echo '<script> loadData("' . $type .'",'. $id .','. $numOfImages . '); </script>';
+    ?>
 
-    <div id="openModal" class="modalDialog">
-        <div>
-            <a href="#close" title="Close" class="close">X</a>
-            <div>
-                <table>
-                    <tr>
-                        <td onclick="toggleLeft();"> 
-                            <a href="#openModal"><span class="arrow_left"></span></a> 
-                        </td>
-
-                        <td id="image"></td>
-
-                        <td onclick="toggleRight();"> 
-                            <a href="#openModal"><span class="arrow_right"></span></a> 
-                        </td>
-                    </tr>
-                </table>
-
-            </div>
-        </div>
-    </div>';
-
-echo '<script> loadData("' . $type .'",'. $id .','. $numOfImages . '); </script>';
-
-?>
-
+    <?php require 'templates/footer.php' ?>
 
 </body>
 </html>
--- events.php
+++ events.php
@@ -1,49 +1,48 @@
 <!doctype html>
 <html>
 <head>
-    <link rel="stylesheet" href="/costaRicaIsrael/css/main.css">
-    <link rel="stylesheet" href="/costaRicaIsrael/css/events.css">
-    <?php require './con_util.php' ?>
     <meta charset="utf-8" />
     <title>Costa Rica Israel</title>
+    <link rel="stylesheet" href="/costaRicaIsrael/css/main.css">
+    <link rel="stylesheet" href="/costaRicaIsrael/css/events.css">
+
+    <script src="/costaRicaIsrael/js/events.js"></script>
+    <?php require './con_util.php' ?>
 </head>
 
 <body>
-
-    <script src="/costaRicaIsrael/js/events.js"></script>
-    <script src="/costaRicaIsrael/js/nav_bar.js"></script>
+    <?php require 'templates/navbar.php'?>
 
     <div id="container_center">
         <div class="container">
             <p class="events_tablesName"> Events Tables </p>
 
-<!-- Events table -->
+    <!-- Events table -->
             <table class="eventsTables" id="general_events" >
                 <script> eventsHeader(); </script>
             <?php
                 $con;
                 $query = "SELECT * FROM events_en";
                 $query_data = set_con_get_query_data($con,$query);
-                
 
                 while($row = mysqli_fetch_row($query_data)) {
-                    $new_row = array(  
+                    $new_row = array(
                                     "id"            => $row[0],
                                     "date"          => $row[1],
                                     "name"          => $row[2],
                                     "description"   => $row[3]);
-                    echo '<tr onmousedown="open_eventPage('.$new_row["id"]. ');">
-                        <td> ' . $new_row["date"] . ' </td> 
-                        <td> ' . $new_row["name"] . ' </td> 
-                        <td> ' . $new_row["description"] . ' </td> 
-                        <td> click here </td> 
-                        </tr>';
+                    echo   '<tr onmousedown="open_eventPage('.$new_row["id"]. ');">
+                            <td> ' . $new_row["date"] . ' </td>
+                            <td> ' . $new_row["name"] . ' </td>
+                            <td> ' . $new_row["description"] . ' </td>
+                            <td> click here </td>
+                            </tr>';
                 }
             ?>
             </table>
             <br><br>
-            
-<!-- Meetings table -->
+
+    <!-- Meetings table -->
             <p class="events_tablesName"> Meeting Tables </p>
             <table class="eventsTables" id="meetings_events">
                 <script> eventsHeader(); </script>
@@ -51,28 +50,28 @@
                 $con;
                 $query = "SELECT * FROM meetings_en";
                 $query_data = set_con_get_query_data($con,$query);
-                
 
                 while($row = mysqli_fetch_row($query_data)) {
-                    $new_row = array(  
+                    $new_row = array(
                                     "id"            => $row[0],
                                     "date"          => $row[1],
                                     "name"          => $row[2],
                                     "description"   => $row[3]);
-                    echo '<tr onmousedown="open_meetingPage('.$new_row["id"]. ');">
-                        <td> ' . $new_row["date"] . ' </td> 
-                        <td> ' . $new_row["name"] . ' </td> 
-                        <td> ' . $new_row["description"] . ' </td> 
-                        <td> click here </td> 
-                        </tr>';
+                    echo   '<tr onmousedown="open_meetingPage('.$new_row["id"]. ');">
+                            <td> ' . $new_row["date"] . ' </td>
+                            <td> ' . $new_row["name"] . ' </td>
+                            <td> ' . $new_row["description"] . ' </td>
+                            <td> click here </td>
+                            </tr>';
                 }
+
             ?>
             </table>
-    
+
         </div>
     </div>
 
-    <script src="/costaRicaIsrael/js/footer.js"></script>
+    <?php require 'templates/footer.php' ?>
 
 </body>
 </html>
