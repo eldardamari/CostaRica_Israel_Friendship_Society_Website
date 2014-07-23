d433156f525e5aa459973d29cc4aefa4422955a7
drorventura <ventura2005@gmail.com>
2014-07-23 09:26:13 +0300

    Added - events can be added - event photos can be removed

--- edit_events.php
+++ edit_events.php
@@ -13,11 +13,12 @@
 
     <script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
     <script src="js/browser.js"></script>
+    <script src="js/events.js"></script>
     <?php require 'utils/files.php' ?>
     <?php include 'utils/edit_events_functions.php' ?>
 </head>
 
-<body onload="init('','events/events/')">
+<body onload="init('','events/events/');$('#type').attr('value','events');">
     <?php require 'templates/navbarpannel.php'?>
 
     <div id="container_center">
@@ -40,22 +41,57 @@
                         header('refresh:1;url=edit_events.php');
                     }
                     if(isset($_POST['add'])) {
-                        var_dump($_POST);
+
+                        if ($_FILES['pictures']['error'] != UPLOAD_ERR_NO_FILE) {
+                            $uploaded_pictures = check_multiple_files('pictures');
+                        }
+
+                        $num_of_pictures = $uploaded_pictures['num_of_pictures'];
+
+                        $date = $_POST['date'];
+                        $eventName = htmlspecialchars($_POST['eventName']);
+
+                        $description = htmlspecialchars($_POST['description']);
+                        $description = filter_var($description,FILTER_SANITIZE_STRING);
+
+                        $text = htmlspecialchars($_POST['text']);
+                        $text = filter_var($text,FILTER_SANITIZE_STRING);
+
+                        $id = addEventToDBAndGetBackID($date, $eventName, $description, $text, $num_of_pictures);
+
+                        $filesMoved = 0;
+                        $eventType = $_POST['type'];
+                        if($id) {
+                            $directoryPath   = "img/events/".$eventType."/".$id;
+                            $filesMoved = uploadPhotosToEvent($uploaded_pictures, $directoryPath, $num_of_pictures);
+
+                            // in case of update
+                            if($filesMoved != $num_of_pictures)
+                                updateNumOfPics($filesMoved);
+
+                            echo '<p class="form_granted">&emsp;Event added successfully!
+                                    <img src="/costaRicaIsrael/img/icons/green_v.png" height="20" width="20" alt="green_v"/></p>';
+
+                        } else {
+                            echo "<p class='text form_error'>&emsp;
+                                    Failed updating database..please try again.";
+                        }
+
                     }
-
+                    include 'templates/add_event_form.php';
             ?>
-
+                <br><br>
                 <div clase="viewer">
                     <div class="browser">
                         <div id="directoryContent" class="contents">&nbsp;</div>
                     </div>
                     <div class="preview">
                         <img id="imagePreview" class="imagePreview" />
-                        <br><br>
+                        <br>
                         <?php echo '<p id="imageName" class="text_center"></p> '?>
-                        <form id="deletePhoto" method="post" action="edit_events.php">
+                        <form id="deletePhoto" class="general_form" method="post" action="edit_events.php">
                             <button type="submit" class="btn_default" id="action" name="delete">
-                                Remove <span class="btn_icon icon_delete"></span>
+                                <span class="btn_icon icon_delete"></span> Remove
                             </button>
                         </form>
                     </div>
--- utils/edit_events_functions.php
+++ utils/edit_events_functions.php
@@ -19,3 +19,78 @@
 
         return $result;
     }
+
+    function addEventToDBAndGetBackID($date, $eventName, $description, $text, $num_of_pictures) {
+        $id = 0;
+        $con = makeConnection();
+
+        $sql = "INSERT INTO events_en (date,name,description,text,number_of_pics)
+                                    VALUES (:date, :name, :description, :text, :number_of_pics)";
+
+        try {
+            $statement = $con->prepare($sql);
+            $statement->bindParam(':date' ,$date);
+            $statement->bindParam(':name' ,$eventName, PDO::PARAM_STR);
+            $statement->bindParam(':description' ,$description, PDO::PARAM_STR);
+            $statement->bindParam(':text' ,$text, PDO::PARAM_STR);
+            $statement->bindParam(':number_of_pics' ,$num_of_pictures, PDO::PARAM_INT);
+            $statement->execute();
+
+            $id = $con->lastInsertId();
+
+        } catch (PDOException $e) {
+            sendErrorToAdmin("DB ERROR: ".$e->getCode(), $e->getMessage());
+            echo "<p class='text form_error'>&emsp;
+                                    Failed updating database..please try again.";
+        }
+
+        return $id;
+    }
+
+    //Uploading pictures
+    function uploadPhotosToEvent($uploaded_pictures, $directoryPath, $num_of_pictures) {
+
+        if (!is_dir($directoryPath)) {
+            mkdir($directoryPath, 0777, true);
+        } else {
+            echo "<p class='text form_error'>&emsp;
+                    event's directory already exist.";
+        }
+
+        $msg = "";
+        $count = 0;
+        for($i = 0 ; $i < $num_of_pictures ; $i++) {
+            $tmp_path = $uploaded_pictures['pictures_tmp'][$i];
+            $filename = $uploaded_pictures['pictures_name'][$i];
+            $pictures_uniqid = $uploaded_pictures['pictures_uniqid'][$i];
+
+            if (!move_uploaded_file($tmp_path,$directoryPath.'/'.$pictures_uniqid)) {
+                $msg .= '&#149 '. $filename .' <br>';
+            } else {
+                $count++;
+            }
+        }
+
+        echo ($msg != "") ? '<p class="text form_error">&emsp;
+                             The following files failed to be upload:
+                             <br>'.$msg.'</p>' : '';
+
+        return $count;
+    }
+
+    function updateNumOfPics($filesMoved) {
+
+        $con = makeConnection();
+        try {
+            $sql_count = "UPDATE events_en SET number_of_pics=:count WHERE id=:id";
+            $statement_count = $con->prepare($sql_count);
+            $statement_count->bindParam(':count', $count, PDO::PARAM_INT);
+            $statement_count->bindParam(':id', $id, PDO::PARAM_INT);
+            $statement_count->execute();
+        } catch (PDOException $e) {
+            sendErrorToAdmin("DB ERROR - Updating event id: $id num_of_pics to $filesMoved",
+                $e->getMessage());
+            echo "<p class='text form_error'>&emsp;
+                                    Failed updating database..please try again.";
+        }
+    }
