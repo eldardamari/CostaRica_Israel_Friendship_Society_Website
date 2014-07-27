797be5484d0c81980881ee6dc3f81b82ca056c96
Eldar Damari <eldar.damari@gmail.com>
2014-07-27 15:46:30 +0300

    EDIT - email headers supports From. Reply To. and body text type(html). private message now with style - need to check if data is sanitize

--- utils/email.php
+++ utils/email.php
@@ -4,6 +4,18 @@
         return "costarica.israel.association@gmail.com";
     }
 
+    function getAssociationMail() {
+        /*return "costarica.isr@gmail.com";*/
+        return "test_need_to_chgne@rgmail.com";
+    }
+
+    function getMailHeaders($email) {
+
+        return 'From: Costa-Rica Israel Association <'.getAssociationMail().'> ' . "\r\n" .
+            'Reply-To: '.$email. "\r\n" .
+            'Content-Type: text/html; charset=utf-8 '    . "\r\n";
+    }
+
     function spamCheck($field) {
         // Sanitize e-mail address
         $field=filter_var($field, FILTER_SANITIZE_EMAIL);
@@ -37,15 +49,35 @@
                     '</td></tr></table></div></body></html>';
 
         $adminEmail = getAdminMail();
-        return mail($email, $subject, $content, "From: $adminEmail\r\nContent-Type: text/html; charset=ISO-8859-1\r\n");
+        return mail($email, $subject, $content, getMailHeaders(getAssociationMail()));
     }
 
     function sendMailToMember($memberEmail, $subject, $content, $fromEmail) {
         $replyInfo = "\n\n\te-mail to reply sender: $fromEmail";
-        return mail($memberEmail, $subject, $content.$replyInfo, "From: $fromEmail\r\nReply-To: $fromEmail\r\n");
+
+        $content =  '<html><body>'.
+                '   <div style="text-align: left; font-size: 200%; font-weight: 550; color: #348017;">
+                    Hi, you have received a private message:</div><br>
+                             <div>
+                    <table width=80% align="center" style="border-style:solid; border-width:medium; border-color:#E8E8E8;"> <tr> <td>
+                        <div style="text-align: left; font-size: 100%; font-weight: 225;">
+                            <p><b><u>Summary</u></b></p></div>
+                        <ul>
+                            <li><p><b>From:</b> '.$fromEmail.'</p></li>'.
+                            '<li><p><b>Subject:</b> '.$subject.'</p></li>'.
+                            '<li><p><b>Message:</b> '.$content.'</p></li>'.
+                            '<li><p><b>Click to Reply:</b> 
+                            <a href="mailto:'.$fromEmail.'?Subject=Re:.'.$subject .' target="_top"> Click Here</a>
+                            </p></li>'.
+                        '</ul>'.
+                    '</td></tr></table></div></body></html>';
+        /*return mail($memberEmail, $subject, $content.$replyInfo, "From: $fromEmail\r\nReply-To: $fromEmail\r\n");*/
+        return mail($memberEmail, $subject, $content, getMailHeaders($fromEmail));
     }
 
     function sendErrorToAdmin($subject,$message) {
         $adminEmail = getAdminMail();
-        return mail($adminEmail, $subject, $message, "From: $adminEmail\r\n");
+        /*return mail($adminEmail, $subject, $message, "From: $adminEmail\r\n");*/
+        return mail($adminEmail, $subject, $message, getMailHeaders(getAdminMail()));
+ 
     }
