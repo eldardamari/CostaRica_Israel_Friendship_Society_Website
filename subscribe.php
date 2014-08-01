889b9e88a4abfe89305a99f61ce16ff537af84aa
Eldar Damari <eldar.damari@gmail.com>
2014-08-01 12:00:29 +0300

    ADD - new tables in db newsletter and buelltin - on subscribe page new skeleton for data - still in progress

--- subscribe.php
+++ subscribe.php
@@ -5,6 +5,8 @@
     <title>Costa Rica Israel</title>
     <link rel="stylesheet" href="/costaRicaIsrael/css/main.css">
     <link rel="stylesheet" href="/costaRicaIsrael/css/form.css">
+    <link rel="stylesheet" href="/costaRicaIsrael/css/events.css">
+    <link rel="stylesheet" href="/costaRicaIsrael/css/subscribe.css">
 
     <script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
     <script src="/costaRicaIsrael/js/subscribe.js"></script>
@@ -18,12 +20,86 @@
 
     <div id="container_center">
         <div class="container">
-            <h2>Subscribe</h2>
+
+            <?php require_once 'utils/subscribe_functions.php'; ?>
+
+            <div class="topics_medium">Bulletin & Newspaper</div><br>
+
+            <div class="publications_box">
+                <table class="eventsTables publications" id="newsletter_table">
+                    <tr> <th class="enameCol" colspan="2"> Newletter (Hebrew) </th></tr> 
+                    <tbody>
+<?php
+
+                $con = makeConnection();
+
+                $query = "SELECT * FROM newsletter
+                          ORDER BY year DESC , month DESC
+                          LIMIT 0 , 6;";
+                
+                $result = prepareAndExecuteQuery($con,$query);
+
+                foreach($result as $row)
+                    echo '<tr> <td colspan="2">'.$row["month"].'  </td></tr>';
+                    /*echo '<tr> <td colspan="2">'.$row["year"].' '.get_month($row["month"]).'  </td></tr>';*/
+                
+                if(sizeof($result) < 6) {
+                    for ($i=0 ; $i< 6-sizeof($result) ;$i++) 
+                        echo '<tr> <td colspan="2"></td></tr>';
+                }
+?>
+                </tbody>
+                <tfoot>
+                    <tr>
+                        <td><button id="prev" value="-6" disabled> prev</button></td>
+                        <td><button id="next" value="6"> next</button></td>
+                    </tr>
+                  </tfoot>
+                </table>
+            
+
+                <table class="eventsTables publications" id="bulletin_table">
+                    <tr> <th class="enameCol" colspan="2"> Bulletin (Spanish) </th></tr>
+                    <tbody>
+<?php
+
+                $con = makeConnection();
+
+                $query = "SELECT * FROM bulletin
+                          ORDER BY year DESC , month DESC
+                          LIMIT 0 , 6;";
+                
+                $result = prepareAndExecuteQuery($con,$query);
+
+                foreach($result as $row)
+                    echo '<tr> <td colspan="2">'.$row["month"].'  </td></tr>';
+                    /*echo '<tr> <td colspan="2">'.$row["year"].' '.get_month($row["month"]).'  </td></tr>';*/
+                
+                if(sizeof($result) < 6) {
+                    for ($i=0 ; $i< 6-sizeof($result) ;$i++) 
+                        echo '<tr> <td colspan="2"></td></tr>';
+                }
+?>
+                    </tbody>
+
+                    <tfoot>
+                    <tr>
+                        <td><button id="prev" value="-6" disabled> prev</button></td>
+                        <td><button id="next" value="6"> next</button></td>
+                    </tr>
+                </tfoot>
+            </table>
+
+            </div>
+
+            <br><br><br><br><hr><br>
+
+
+            <div class="topics_medium">Subscribe</div>
 
             <?php
                 $showForm = true;
 
-                require_once 'utils/subscribe_functions.php';
 
                 if($showForm) {
 
